f1ab76e7a713adfea3eb7929e2f380937aa191a0
syukur91 <syukur.jtk09@gmail.com>
2017-08-24 20:51:54 +0700

    Enhance downloadImage

--- app/src/main/java/com/slidead/app/slidead/helpers/DownloadHelper.java
+++ app/src/main/java/com/slidead/app/slidead/helpers/DownloadHelper.java
@@ -17,30 +17,34 @@ import java.util.Arrays;
 
 public class DownloadHelper extends Activity {
 
-    final String url = "http://cdn3.nflximg.net/images/3093/2043093.jpg";
-    private BroadcastReceiver mDLCompleteReceiver;
-
-
     public static void  downloadImage(String url, Context c) {
 
         DownloadManager.Request request = null;
+        File direct = new File(Environment.getExternalStorageDirectory() + "/lookads");
+
+        if (!direct.exists()) {
+            direct.mkdirs();
+        }
 
         try {
             request = new DownloadManager.Request(Uri.parse(url));
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_HIDDEN);
+            request.setVisibleInDownloadsUi(false);
         } catch (IllegalArgumentException e) {
             Log.d("Slider Demo","Error" + e.getMessage());
         }
 
-          /* allow mobile and WiFi downloads */
+        /* allow mobile and WiFi downloads */
         request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI);
-        request.setTitle("DM Example");
-        request.setDescription("Downloading file");
+        request.setDestinationInExternalPublicDir("/lookads", "2043093.jpg");
+//        request.setTitle("DM Example");
+//        request.setDescription("Downloading file");
 
          /* we let the user see the download in a notification */
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
-                /* Try to determine the file extension from the url. Only allow image types. You
-                 * can skip this check if you only plan to handle the downloaded file manually and
-                 * don't care about file managers not recognizing the file as a known type */
+//        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
+        /* Try to determine the file extension from the url. Only allow image types. You
+         * can skip this check if you only plan to handle the downloaded file manually and
+         * don't care about file managers not recognizing the file as a known type */
         String[] allowedTypes = {"png", "jpg", "jpeg", "gif", "webp"};
         String suffix = url.substring(url.lastIndexOf(".") + 1).toLowerCase();
 
@@ -54,18 +58,9 @@ public class DownloadHelper extends Activity {
 
           /* set the destination path for this download */
 //        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS +File.separator + "2043093", name + "." + suffix);
-
-        File direct = new File(Environment.getExternalStorageDirectory()
-                + "/dhaval_files");
-
-        if (!direct.exists()) {
-            direct.mkdirs();
-        }
-
         final DownloadManager dm = (DownloadManager) c.getSystemService(Context.DOWNLOAD_SERVICE);
                 /* this is our unique download id */
         final long DL_ID = dm.enqueue(request);
-
 
 
     }
